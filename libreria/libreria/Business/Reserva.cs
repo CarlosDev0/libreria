8c7c8d9928bab03e4cdbd28fd866139c10f9b8c7
carlosas <carlosabs@algartech.com>
2021-12-14 21:55:19 -0500

    Inicial Libreria

---
+++ libreria/libreria/Business/Reserva.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreria.Business
+{
+    public class Reserva
+    {
+        public int GetReserva()
+        {
+            return 1;
+        }
+
+    }
+}
