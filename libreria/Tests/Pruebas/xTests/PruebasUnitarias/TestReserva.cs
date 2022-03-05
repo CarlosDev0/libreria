d72dad85b4ddb7157b5b732d8716378b1cb48c25
Carlos Alberto Sanchez <carlossan@gmail.com>
2022-03-04 21:42:06 -0500

    Proyecto de Pruebas xUnit

---
+++ libreria/Tests/Pruebas/xTests/PruebasUnitarias/TestReserva.cs
@@ -0,0 +1,26 @@
+﻿using Libreria.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xTests.PruebasUnitarias
+{
+    public class TestReserva
+    {
+
+        //public void Setup()
+        //{
+        //}
+
+        [Fact]
+        public void PruebaReserva()
+        {
+            Reserva reservas = new Reserva();
+            int rta = reservas.GetReserva();
+            Assert.Equal(expected: 1, rta);
+            //"No se obtivo la respuesta esperada");
+            
+        }
+    }
+}
